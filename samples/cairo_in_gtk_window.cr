caad4479a8b08d069cd788dd5e94574440d574b5
viachpaliy <viachpaliy@.ukr.net>
2019-11-25 23:39:40 +0200

    add sample for Gtk::Window

---
+++ samples/cairo_in_gtk_window.cr
@@ -0,0 +1,32 @@
+require "gobject/gtk/autorun"
+require "../src/cairo"
+
+class CairoApp
+  @window : Gtk::Window
+  
+
+  delegate show_all, to: @window
+
+  def initialize
+    @window = Gtk::Window.new
+    @window.title = "Simple drawing"
+    @window.resize 250,150
+    @window.connect "destroy", &->Gtk.main_quit
+    darea = Gtk::DrawingArea.new
+    darea.connect "draw",&->drawfun 
+    @window.add darea
+  end
+
+  def drawfun
+    context = Gdk.cairo_create(@window.window.not_nil!)
+    context.set_source_rgb(0, 100, 0) 
+    context.select_font_face("Sans", Cairo::FontSlant::NORMAL , Cairo::FontWeight::NORMAL)
+    context.font_size=40 
+    context.move_to(10,50)
+    context.show_text("Cairo works!!!")
+  end
+
+end
+
+app=CairoApp.new
+app.show_all

03690b582400b5df7611cd214ad1ebec15326c1b
viachpaliy <viachpaliy@.ukr.net>
2019-11-30 00:59:53 +0200

    add sample

---
+++ samples/transparent_rect.cr
@@ -0,0 +1,33 @@
+require "gobject/gtk/autorun"
+require "../src/cairo"
+
+class CairoApp
+  @window : Gtk::Window
+ 
+  delegate show_all, to: @window
+
+  def initialize
+    @window = Gtk::Window.new
+    @window.title = "Radial gradient"
+    @window.resize 500,150
+    @window.connect "destroy", &->Gtk.main_quit
+    darea = Gtk::DrawingArea.new
+    darea.connect "draw",&->drawfun 
+    @window.add darea
+  end
+
+  def drawfun
+    context = Gdk.cairo_create(@window.window.not_nil!)
+    i=1
+    while i < 10
+      context.set_source_rgba(0, 0, 1, i*0.1)
+      context.rectangle(Float64.new(50*i), 20.0, 40.0, 40.0)
+      context.fill
+      i = i + 1
+    end
+  end
+
+end
+
+app=CairoApp.new
+app.show_all
